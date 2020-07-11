0c9bcbb8538a209a4936f695db1b5d7dd5939386
HAMZAdeve <66545200+HAMZAdeve@users.noreply.github.com>
2020-07-11 20:35:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class BasicFreggersBot(FreggersBot):
 			log_error(e)
 
 bots = [
-	BasicFreggersBot('test1234','babaali2012'),
+	BasicFreggersBot('reagirl','babaali2012'),
 	BasicFreggersBot('Username2', 'Password2'),
 	BasicFreggersBot('Username3', 'Password3'),
 ]
